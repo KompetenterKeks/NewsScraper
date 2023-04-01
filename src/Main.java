0cb0ab1deb00cdde415f23bc2f0afd14a57263af
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-04-01 15:46:37 +0200

    Did some refactoring and put API accessing within its own method

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,46 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.net.URI;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        HttpClient client = HttpClient.newHttpClient();
+        JSONParser parser = new JSONParser();
 
-        int date = 230330;
+        Integer date = 230330;
+
+        JSONArray news = getNewsfeed(client, parser, date);
+
+        JSONObject test = (JSONObject) news.get(0);
+        System.out.println(test.get("sophoraId"));
+    }
+
+    private static JSONArray getNewsfeed(HttpClient client, JSONParser parser, Integer date) throws ParseException {
+        // Prepare API URL with the right date
         String URL = String.format("https://www.tagesschau.de/api2/newsfeed-101~_date-%d.json", date);
 
-        HttpClient client = HttpClient.newHttpClient();
+        // Create a new HTTP request for accessing the API
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(URL))
                 .header("accept", "application/json")
                 .build();
 
+        // Parse and return the newsfeed in form of a JSON Array
         String resp = client.sendAsync(request, BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .join();
 
-
-        JSONParser parser = new JSONParser();
-
-        try {
-			
-            JSONObject data = (JSONObject) parser.parse(resp);
-			
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        JSONObject data = (JSONObject) parser.parse(resp);
+        return (JSONArray) data.get("news");
     }
 }
+
+
 // Cursor Parking
 // __________
 // |  |  |  |
